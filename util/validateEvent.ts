d88b91f6a58229294fed1660fff903082354f929
Per Sandström <pg.sandstrom@gmail.com>
2020-11-15 16:10:42 +0100

    Validate max length of strings

--- util/validateEvent.ts
+++ util/validateEvent.ts
@@ -1,7 +1,7 @@
 import { DiceRoll, EventParticipant, EventParticipantRollIncomplete } from '../types'
 
 export const validateEventName = (title?: string): title is string => {
-  return title !== undefined && title.length > 0
+  return title !== undefined && title.length > 0 && title.length < 200
 }
 
 export const validateRolls = (rolls: DiceRoll[] | undefined) => {
@@ -12,6 +12,7 @@ export const validateRoll = (roll: Partial<DiceRoll>) => {
   return (
     roll.name !== undefined &&
     roll.name.length > 0 &&
+    roll.name.length < 500 &&
     roll.sides !== undefined &&
     roll.sides > 0 &&
     roll.sides < 101
